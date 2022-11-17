71d3c5d276cdcb3044ae4d237b18fc992252bff8
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-17 11:27:27 -0300

    add: sucess response and error

---
+++ backend/src/shared/error.ts
@@ -0,0 +1,8 @@
+export class ErrorObject extends Error {
+	status: number
+	constructor (message: string, status: number) {
+		super()
+		this.message = message
+		this.status = status || 500
+	}
+}
---
+++ backend/src/shared/sucessResponse.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express'
+
+type response = {
+  res: Response
+  status: number
+  message: string
+  token: string
+  // eslint-disable-next-line
+  data: any
+}
+
+const sucessResponse = ({ res, status = 200, message, data, token } : response) => {
+	res.status(status).json({
+		status,
+		message,
+		data,
+		token
+	})
+}
+
+export default sucessResponse
