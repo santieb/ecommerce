d5405cbfe17c842cf81c28ccd8c2bd9a76ec0605
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-18 08:59:09 -0300

    add: verify util - bcrypt

--- backend/src/auth/utils/bcrypt.ts
+++ backend/src/auth/utils/bcrypt.ts
@@ -1,5 +1,7 @@
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 
 const encrypt = async (password: string) => await hash(password, 8)
 
-export { encrypt }
+const verify = async (pass: string, passHash: string) => await compare(pass, passHash)
+
+export { encrypt, verify }
