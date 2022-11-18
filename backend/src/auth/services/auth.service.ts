53f1691c8c8c692ac000d666d3dd7227be3b26ea
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-18 09:00:36 -0300

    add: userlogin service

--- backend/src/auth/services/auth.service.ts
+++ backend/src/auth/services/auth.service.ts
@@ -1,7 +1,9 @@
 import db from '../../config/database'
 import { ErrorObject } from '../../shared/helpers/error'
-import { User } from '../types/user.type'
-import { encrypt } from '../utils/bcrypt'
+import { User } from '../types-interfaces/user.interface'
+import { Auth } from '../types-interfaces/auth.interface'
+import { encrypt, verify } from '../utils/bcrypt'
+import { generateToken } from '../utils/jwt'
 
 const registerUser = async ({ email, name, password } : User) => {
 	const userExists = await db.user.findUnique({ where: { email } })
@@ -18,4 +20,19 @@ const registerUser = async ({ email, name, password } : User) => {
 	})
 }
 
-export default { registerUser }
+const loginUser = async ({ email, password } : Auth) => {
+	const user = await db.user.findUnique({ where: { email } })
+	if (!user) throw new ErrorObject('user already exists', 403)
+
+	const { id, isAdmin, password: passwordHashed } = user
+
+	const isValid = await verify(password, passwordHashed)
+	if (!isValid) throw new ErrorObject('password is invalid', 403)
+
+	const token = await generateToken(id, isAdmin)
+	user.password = ''
+
+	return { user, token }
+}
+
+export default { registerUser, loginUser }
