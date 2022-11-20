0fd789f1ade82adf3ffafb4d68870fe5c4f9bd37
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-20 08:53:45 -0300

    add: ctrl and service

--- server/src/products/controllers/product.controller.ts
+++ server/src/products/controllers/product.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express'
 import sucessResponse from '../../shared/helpers/sucessResponse'
+import { ErrorObject } from '../../shared/helpers/error'
+import productService from '../services/product.service'
 
-const getProducts = async ({ body }: Request, res: Response) => {
+const getProducts = async (req: Request, res: Response) => {
   try {
+    const data = await productService.getProducts()
+    if (data.length === 0) throw new ErrorObject('Not Found', 404)
+
     sucessResponse({
       res,
       status: 200,
-      message: 'products successfully obtained'
+      message: 'Products successfully obtained',
+      data
     })
   } catch (err) {
     res.status(400).send(err)
