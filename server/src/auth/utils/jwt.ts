84d669452d7c29fc5f0139af6acda6be6b1e849d
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-21 09:04:01 -0300

    fix: jwt payload

--- server/src/auth/utils/jwt.ts
+++ server/src/auth/utils/jwt.ts
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken'
 import { config } from '../../config/index'
 const { secretKey } = config
 
-const generateToken = (id: string, role: boolean): string => jwt.sign({ id, role }, secretKey, { expiresIn: 60 * 60 })
+const generateToken = (id: string, isAdmin: boolean): string => jwt.sign({ id, isAdmin }, secretKey, { expiresIn: 60 * 60 })
 
 export { generateToken }
