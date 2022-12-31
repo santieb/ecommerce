b5409a81f0c33ae120900b28804e8ccb7b69950c
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-12-31 16:13:33 -0300

    add: db and updates scripts for windowsd

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -8,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
   ],
 })
