586b7cb650c32d8f87bb808b1820d769fc066eed
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2023-01-02 22:19:25 -0300

    add: create and allcategories ctrl

--- server/src/categories/categories.module.ts
+++ server/src/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './services/categories.service';
 import { CategoriesController } from './controllers/categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesEntity } from './entities/categories.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoriesEntity])],
   providers: [CategoriesService],
   controllers: [CategoriesController],
 })
--- server/src/categories/controllers/categories.controller.ts
+++ server/src/categories/controllers/categories.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  UseGuards,
+  HttpException,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/users/jwt.auth.guard';
+import { CategoriesService } from '../services/categories.service';
+import { CreateCategoryDto } from '../dto/createCategory.dto';
+import { CategoriesEntity } from '../entities/categories.entity';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createCategory(
+    @Body() newCategory: CreateCategoryDto,
+  ): Promise<CategoriesEntity | HttpException> {
+    return this.categoriesService.createCategory(newCategory);
+  }
+
+  @Get()
+  allCategories() {
+    return this.categoriesService.allCategories();
+  }
+}
