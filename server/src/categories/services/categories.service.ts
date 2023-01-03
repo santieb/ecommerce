e8211b63912d4515100a42c9bd2ad2271f8144a0
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2023-01-02 22:18:39 -0300

    add: createCategory and allCategories

--- server/src/categories/services/categories.service.ts
+++ server/src/categories/services/categories.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CategoriesEntity } from '../entities/categories.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCategoryDto } from '../dto/createCategory.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(CategoriesEntity)
+    private categoryRepository: Repository<CategoriesEntity>,
+  ) {}
+
+  async createCategory(category: CreateCategoryDto) {
+    const categoryFound = await this.categoryRepository.findOne({
+      where: { name: category.name },
+    });
+
+    if (categoryFound)
+      return new HttpException('Category already exists', HttpStatus.CONFLICT);
+
+    const newCategory = this.categoryRepository.create(category);
+    return this.categoryRepository.save(newCategory);
+  }
+
+  allCategories(): Promise<object | HttpException> {
+    return this.categoryRepository.find();
+  }
+}
