ba382f2ecb3267502e9b402bbe0d06a792e2d3c8
Thomas Barreto <thomi.b137@gmail.com>
2023-01-04 00:05:09 -0300

    main structure

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -10,7 +10,19 @@ export default function RootLayout({
       <head>
         <title>Ecommerce</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <div className="fixed top-4 right-4">
+          </div>
+          <div className="w-full bg-orange-300 text-5xl">
+          <span className="text-orange-600">COFFE</span> 
+            <h1 className="text-white font-extrabold text-center uppercase">
+              La mejor comida casera
+              <br/>
+              <span className="text-orange-600">caseritos</span> 
+            </h1>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
