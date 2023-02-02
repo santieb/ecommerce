110a0e73c60c31442eedfe7f1845fd27668c8d1e
Thomas Barreto <thomi.b137@gmail.com>
2023-02-01 23:50:30 -0300

    fetch categories

--- server/src/orders/controller/orders.controller.ts
+++ server/src/orders/controller/orders.controller.ts
@@ -1,14 +1,7 @@
 import { Controller, Post, UseGuards, Body, Req } from '@nestjs/common';
-<<<<<<< HEAD
-=======
 import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
-<<<<<<< HEAD
->>>>>>> 5bf39710b24e88a0852003b3d11d5960678e94e3
-import { JwtAuthGuard } from 'src/users/jwt.auth.guard';
-=======
 import { RolesGuard } from 'src/users/helpers/role.guard';
 import { JwtAuthGuard } from '../../users/helpers/jwt-auth.guard';
->>>>>>> 3f7e1e3a7bfb04667234a1dfac2d5eef4e6306c9
 import { AddOrderDto } from '../dto/addOrder.dto';
 import { OrdersService } from '../service/orders.service';
 
@@ -20,8 +13,6 @@ export class OrdersController {
   @Post()
   addOrder(@Body() newOrder: AddOrderDto, @Req() req) {
     return this.ordersService.addOrder(newOrder, req.user);
-<<<<<<< HEAD
-=======
   }
 
   @UseGuards(JwtAuthGuard)
@@ -34,6 +25,5 @@ export class OrdersController {
   @Get()
   allOrders() {
     return this.ordersService.getAllOrders();
->>>>>>> 5bf39710b24e88a0852003b3d11d5960678e94e3
   }
 }
