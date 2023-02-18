9dbcc410b2c60af3cb1eb05fcff406fe03369fc9
Thomas Barreto <thomi.b137@gmail.com>
2023-02-18 19:27:27 -0300

    fix styles

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useUserStore } from '../state/Products'
 import LoginModal from './LoginModal'
 import RegisterModal from './RegisterModal'
 
 const NavBar = () => {
+ 
+
+  const { getUser, user } = useUserStore();
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+    <nav className="w-screen navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
       <div className="px-6 w-full flex flex-wrap items-center justify-between">
         <div className="flex items-center">
           <Link className="navbar-brand text-orange-600 " href="/">
